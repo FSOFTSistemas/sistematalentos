d8a4070f2058421b84896137d176901c72509afc
jlalvescarvalho <lucianoalves413@gmail.com>
2025-05-22 18:54:48 -0300

    feat: adicionado criação de admine masterno seed

---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Criar usuário Master
+        $master = User::firstOrCreate(
+            ['email' => 'master@ecclesia.com'],
+            [
+                'name' => 'Master Admin',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+        if (!$master->hasRole('master')) {
+            $master->assignRole('master');
+        }
+
+        // Criar usuário Admin
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@ecclesia.com'],
+            [
+                'name' => 'Administrador',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+        if (!$admin->hasRole('admin')) {
+            $admin->assignRole('admin');
+        }
+    }
+}
